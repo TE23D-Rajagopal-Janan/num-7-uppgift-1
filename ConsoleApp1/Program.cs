e3a1fd9550f13fda3b55ad7b81310f8e2c717a47
Janan Rajagopal <Janan.Rajagopal@elev.ga.ntig.se>
2024-11-15 10:56:42 +0100

    ljhvkhv

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿int x = 6;
+int y = 3;
+
+if (x >= y)
+{
+    Console.WriteLine("Hello,World"); 
+}
+
+string name;
+string password;
+Console.WriteLine("Type in Username");
+name = Console.ReadLine();  
+
+Console.WriteLine("Type in Password");
+password = Console.ReadLine();''
+
+if (name != "kalleanka" && password != "12345" )
+{
+    Console.WriteLine("Wrong password or Wrong Username");
+    
+}
+
+int i = 0;  
+while ( i < 32)
+{
+    Console.WriteLine("Hello,World");
+    i++;
+}
+
+
+
+
+
+Console.ReadLine();
